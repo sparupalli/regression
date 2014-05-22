bc1b3aba77fafeb585e8663524f63ef14986f7a1
Srinivasa Parupalli <srinivasa@sherpasystems.com.au>
2014-05-22 15:58:53 +1000

    Missed commit

---
+++ src/test/java/au/com/sherpa/regression/tests/training/course/CourseCreationTest.java
@@ -0,0 +1,18 @@
+package au.com.sherpa.regression.tests.training.course;
+
+import net.thucydides.core.annotations.Story;
+import net.thucydides.junit.runners.ThucydidesRunner;
+
+import org.junit.runner.RunWith;
+
+import au.com.sherpa.regression.requirements.Application;
+
+@Story(Application.Training.Course.class)
+@RunWith(ThucydidesRunner.class)
+public class CourseCreationTest {
+
+	
+	
+	
+	
+}
